61a1789e755835ea2067020b2e0af69a5f8f4d4a
Erick Jovani Hernandez Hernandez <erick_jhh@hotmail.com>
2021-09-03 13:44:46 -0500

    Se crea el component principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
