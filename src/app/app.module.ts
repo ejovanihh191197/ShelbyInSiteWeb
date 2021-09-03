e9f44281531c94fc7a49d7e8a8a8289951a4c46e
Erick Jovani Hernandez Hernandez <erick_jhh@hotmail.com>
2021-09-03 14:16:40 -0500

    Se crea el componenetepie-pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { PiePaginaComponent } from './shared/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
