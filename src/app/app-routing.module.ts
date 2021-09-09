4b5faa0352977a9d1d3cbb0c50ad0b76af698a1d
Erick Jovani Hernandez Hernandez <erick_jhh@hotmail.com>
2021-09-09 12:31:43 -0500

    Se agrega ruta para quienes somos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 import { SolucionesComponent } from './components/soluciones/soluciones.component';
 
 const routes: Routes = [
   {path:'home', component: PrincipalComponent},
   {path:'soluciones', component: SolucionesComponent},
+  {path:'quienesSomos', component: QuienesSomosComponent},
   {path:'**', redirectTo: 'home'}
 ];
 
